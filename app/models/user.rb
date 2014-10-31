0b10b046e3221082f82315f69c4ea9a8ec81c552
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-10-31 12:17:47 -0400

    added notification to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ActiveRecord::Base
   has_many :courses, through: :enrollments
   has_many :stories, through: :courses
   has_many :comments
-  has_many :notifications, through: :noteable
-
+  has_many :notifications
+  def has_notification?
+    self.
+  end
   def is_teacher?
     self.role == "teacher"
   end
