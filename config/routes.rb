6e3be4660eb69c25a6151a2aa611bf0b94fa2499
Thomas Abend <tsabend@gmail.com>
2014-10-31 12:50:30 -0400

    removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@ Rails.application.routes.draw do
   root "courses#index"
   resources :users
   resources :enrollments
+  resources :comments
   resources :courses do
     resources :stories do
-      resources :paragraphs do
-        resources :sentences do
-          resources :comments
-        end
-      end
     end
   end
 end
