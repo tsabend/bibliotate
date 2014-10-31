97880d3c78c17d543dff9e81c97d9badf3c4d3ea
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-10-31 18:17:11 -0400

    seed fix students and teachers lowercase

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,18 +2,21 @@ class StoriesController < ApplicationController
   def index
   end
 
-  def show
-    @story = Story.find(params[:id])
+  def new
+    @course = Course.find(params[:course_id])
+    @story = Story.new
+    # binding.pry
   end
 
   def create
-    @story = Story.new(story_params)
-
-    if @story.save
-      redirect_to @story
-    else
-      redirect_to 'story#index', :flash => { :error => "Please complete all fields."}
-    end
+    binding.pry
+    def self.make(title, author, body, course_id)
+    Story.make(params[:title: title],params[author: author, course_id: course_id])
+    if Story.make(params[:story][:title],params[:story][:author],params[:course_id],)
+    # if @story.save
+    #   redirect_to @story
+    # else
+    #   redirect_to 'story#index', :flash => { :error => "Please complete all fields."}
+    # end
   end
-
 end
