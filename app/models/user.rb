457accd03a86de51d7d609914d7e577ce862b93f
Thomas Abend <tsabend@gmail.com>
2014-10-30 22:25:09 -0400

    Added tests for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   has_many :enrollments
   has_many :courses, through: :enrollments
+  has_many :stories, through: :courses
   has_many :comments
+
+  def is_teacher?
+    self.role == "teacher"
+  end
 end
