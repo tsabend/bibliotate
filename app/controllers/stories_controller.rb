26a902a5e084bbc674f266d9cd614f824a773060
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-10-31 18:24:30 -0400

    basic story creation

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -9,14 +9,11 @@ class StoriesController < ApplicationController
   end
 
   def create
-    binding.pry
-    def self.make(title, author, body, course_id)
-    Story.make(params[:title: title],params[author: author, course_id: course_id])
-    if Story.make(params[:story][:title],params[:story][:author],params[:course_id],)
-    # if @story.save
-    #   redirect_to @story
-    # else
-    #   redirect_to 'story#index', :flash => { :error => "Please complete all fields."}
-    # end
+    if Story.make(params[:story][:title],params[:story][:author],params[:story_body],params[:course_id])
+    @story = Story.last
+    @story
+    else
+      redirect_to 'story#index', :flash => {:error => "Please complete all fields."}
+    end
   end
 end
