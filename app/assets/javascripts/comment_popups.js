3b530e5bc34907860cfffd28d0109021b8e2ec53
Thomas Abend <tsabend@gmail.com>
2014-10-31 14:45:08 -0400

    Added ajax to GET and POST comments

--- app/assets/javascripts/comment_popups.js
+++ app/assets/javascripts/comment_popups.js
@@ -1,23 +1,42 @@
 $(document).ready(function() {
-  $("span").click(function(event){
+  $(".sentence").click(function(event){
     var sentence = $(this)
     fetchPopup(sentence.data('id'), function(template){
-      console.log(template)
-      sentence.prepend(template)
+      sentence.after(template)
+      listenForComment()
     })
-
     popupClose()
   })
 
 })
+function listenForComment() {
+  $(".comment_form").submit(function(e){
+    e.preventDefault()
+    console.log($(this).serialize())
+    submitComment($(this).serialize())
+    $(this).parent().remove()
+  })
+}
 
+
+function submitComment(args) {
+  $.ajax({
+    url: '/comments',
+    type: 'POST',
+    data: args
+  })
+  .done(function(template) {
+    console.log("success");
+    console.log(template);
+  })
+
+}
 
 function fetchPopup(id, callback) {
   $.ajax({
     url: '/comments/' + id,
   })
   .success(callback)
-
 }
 
 function popupClose() {
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,14 @@
 class CommentsController < ApplicationController
 
+  def create
+    puts "FOOBAR"
+    comment = Comment.new(comment_params)
+    comment.user_id = 1
+    comment.save
+    @sentence = Sentence.find(1)
+    redirect_to comment
+  end
+
   def show
     @sentence_id = params[:id]
     @comments = Comment.where(sentence_id: @sentence_id)
