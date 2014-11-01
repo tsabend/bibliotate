ab2d09062950367fa9eb87a0d81433fc8046f5d3
Alex Ung <alexung.ucsb@gmail.com>
2014-11-01 19:28:30 -0400

    Add in new story button to teacher nav

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,7 +1,20 @@
 class EnrollmentsController < ApplicationController
 
   def show
+  end
 
+  def new
+    @enrollment = Enrollment.new
+  end
+
+  def create
+    @enrollment = Enrollment.new(enrollment_params)
+  end
+
+  private
+
+  def enrollment_params
+    params.require(:enrollment).permit([:user_id, :course_id])
   end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,8 +2,8 @@ module SessionsHelper
 
   def current_user
 
-    @user ||= User.find_by(id: session[:user_id])
-    # @user = User.find(10) ## Pls keep this here for now...
+    #@user ||= User.find_by(id: session[:user_id])
+    @user = User.find(1) ## Pls keep this here for now...
   end
 
   def user_logged_in?
