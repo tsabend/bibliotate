a4f87b2918869c4df4880de9e2c91a57d1cfe87d
Alex Ung <alexung.ucsb@gmail.com>
2014-11-02 15:48:26 -0500

    Create enrollment spec in models

--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -1,1 +1,19 @@
-enrollment_spec.rb
+require 'rails_helper'
+
+RSpec.describe Enrollment, :model => :enrollment do
+  describe "associations" do
+
+    it 'belongs to a user' do
+      should belong_to(:user)
+    end
+
+    it "belongs to a course" do
+      should belong_to(:course)
+    end
+
+    it "has many notifications" do
+      should have_many(:notifications)
+    end
+
+  end
+end
