b60df54a40ba3b96c4c37cf32a346d5191bf57e7
Thomas Abend <tsabend@gmail.com>
2014-11-02 17:50:13 -0500

    added ajax for active users

--- app/assets/javascripts/comments_feed.js
+++ app/assets/javascripts/comments_feed.js
@@ -1,9 +1,11 @@
 $(document).on('page:change', function() {
-	listenForComments()
+	listenForRecentComments()
+	listenForMostCommented()
+	listenForActiveUsers
 })
 
 
-function listenForComments() {
+function listenForRecentComments() {
 	setInterval(function() {
 		var storyId = $("[data-card='commentsfeed']").data('story')
 		fetchComments(storyId, function(template) {
@@ -15,6 +17,38 @@ function listenForComments() {
 function fetchComments(id, callback) {
   $.ajax({
     url: '/stories/' + id + '/commentsfeed',
+  })
+  .success(callback)
+}
+
+function listenForMostCommented() {
+	setInterval(function() {
+		var storyId = $("[data-card='mostcommented']").data('story')
+		fetchMostCommented(storyId, function(template) {
+			$("[data-card='mostcommented']").replaceWith(template)
+		})
+	}, 10000)
+}
+
+function fetchMostCommented(id, callback) {
+  $.ajax({
+    url: '/stories/' + id + '/mostcommented',
+  })
+  .success(callback)
+}
+
+function listenForActiveUsers() {
+	setInterval(function() {
+		var storyId = $("[data-card='ActiveUsers']").data('story')
+		fetchActiveUsers(storyId, function(template) {
+			$("[data-card='ActiveUsers']").replaceWith(template)
+		})
+	}, 10000)
+}
+
+function fetchActiveUsers(id, callback) {
+  $.ajax({
+    url: '/stories/' + id + '/ActiveUsers',
   })
   .success(callback)
 }
