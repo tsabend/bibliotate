f742aa886b0682d1f7c89b4f7b63531792866ab9
Alex Ung <alexung.ucsb@gmail.com>
2014-11-02 15:21:00 -0500

    Create comment specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,5 +5,17 @@ RSpec.describe Comment, :type => :model do
     it 'belongs to a sentence' do
       should belong_to(:sentence)
     end
+
+    it 'belongs to a user' do
+      should belong_to(:user)
+    end
+
+    it 'has many hastags' do
+      should have_many(:hashtags)
+    end
+
+    it "has many notifications" do
+      should have_many(:notifications)
+    end
   end
 end
