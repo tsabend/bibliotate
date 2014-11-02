d2ce34b64c3f413eda0608de03559e6d024dfe22
Thomas Abend <tsabend@gmail.com>
2014-11-02 14:30:02 -0500

    Now only shows 5 most recent usues

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -2,6 +2,6 @@ class HashtagsController < ApplicationController
 
 	def show
 		hashtag = Hashtag.find(params[:id])
-		@hashtags = Hashtag.where(tag: hashtag.tag)
+		@hashtags = Hashtag.where(tag: hashtag.tag).first(5)
 	end
 end
