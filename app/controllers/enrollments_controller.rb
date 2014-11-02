322c6df8ac63dcc75f10822c9de32b6f7fdbb23b
Alex Ung <alexung.ucsb@gmail.com>
2014-11-02 00:01:11 -0400

    Course id attached to enrollment

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -15,7 +15,7 @@ class EnrollmentsController < ApplicationController
     @courses = Course.all
     @enrollment = Enrollment.new(enrollment_params)
     @enrollment.user_id = current_user.id
-    @enrollment.course_id = params[:course]
+    @enrollment.course_id = Course.find_by(name: params[:enrollment][:course]).id
     @enrollment.save
     redirect_to '/'
   end
