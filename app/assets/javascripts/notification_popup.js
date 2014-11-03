f9e695628c8840f0835d58f49d934e0c34645251
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-11-02 21:27:17 -0500

    notifications complete

--- app/assets/javascripts/notification_popup.js
+++ app/assets/javascripts/notification_popup.js
@@ -1,16 +1,12 @@
 $( document ).ready(function() {
-$(".notification").on('click', function() {
-console.log("yay")
-        $('ul').not($(this).children("ul")).hide();
-        $(this).children("ul").toggle();
-        $.ajax({
-          url: '/notifications',
-          type: 'POST',
-        })
-        .success(function() {
-          console.log("success");
-        });
+  $(".dropdown.open").on('click', function() {
+    $(".dropdown-menu").toggle()
+    // $.ajax({
+    //   url: '/notifications',
+    //   type: 'POST',
+    // })
+    // .success(function() {
+    //   console.log("success");
+    // });
   })
 });
-
-// "[data-note='yes']"
