c39dd643905d6cc0a6d7da9857b5fa9c405ff20d
Thomas Abend <tsabend@gmail.com>
2014-11-02 21:50:56 -0500

    Moved the hover js to a new file. added CSS for hover_text. It is pushing sentences which i dislike.

--- app/assets/javascripts/comment_popups.js
+++ app/assets/javascripts/comment_popups.js
@@ -1,12 +1,4 @@
 $(document).on('page:change', function() {
-  $(".sentence").hover(function(){
-    var sentence_number = $(this).data("id") - $(".sentence:first").data("id") + 1
-    $(this).after('<sup class="hover-text">' + sentence_number + '</sup>')
-  }, function(){
-    $('.hover-text').remove();
-  })
-
-  listenForComment()
   $(".sentence").click(function(event){
     $('.popup').remove()
     var sentence = $(this)
