520d2a45b6d5e6a1fbf91a9ada7d5b9471b20796
Thomas Abend <tsabend@gmail.com>
2014-11-02 23:38:37 -0500

    removed hack

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,7 @@
 module SessionsHelper
 
   def current_user
-    @user ||= User.find_by(id: 1)
-    # @user ||= User.find_by(id: session[:user_id])
+    @user ||= User.find_by(id: session[:user_id])
   end
 
   def user_logged_in?
