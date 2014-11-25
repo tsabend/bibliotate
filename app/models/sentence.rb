1bd13bc4c220645752ef5d4bd2bdc65a94ca508d
Thomas Abend <tsabend@gmail.com>
2014-11-25 14:15:46 -0800

    Added a callback to provide sentence numbering

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,4 +1,10 @@
 class Sentence < ActiveRecord::Base
   belongs_to :paragraph
   has_many :comments
+  after_create :mark_sentence_number
+
+  def mark_sentence_number
+	  self.number = id - paragraph.story.sentences.first.id + 1
+	  self.save
+	end
 end
